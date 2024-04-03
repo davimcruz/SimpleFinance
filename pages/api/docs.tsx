644dda40ed9866c5ad8284c64c9ce16fad1c4427
Davi <davimachadocruz@gmail.com>
2024-04-03 16:40:53 -0300

    Removido Swagger por incompatibilidade

--- pages/api/docs.tsx
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import swaggerSpec from "../../swagger"
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader("Content-Type", "application/json")
-  res.send(swaggerSpec)
-}
