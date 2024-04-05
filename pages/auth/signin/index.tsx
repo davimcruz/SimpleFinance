f3fa9935e341ba3ad4580530f4cab6bc798221ae
Davi <davimachadocruz@gmail.com>
2024-04-05 05:50:41 -0300

    Atualizado responsividade

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -59,7 +59,7 @@ export default function LoginPage() {
       <div
         className={`${inter.className} flex items-center justify-center h-screen`}
       >
-        <div className="fixed right-4 top-4">
+        <div className="lg:fixed absolute right-4 top-4">
           <ModeToggle />
         </div>
         <Card className="w-[400px] flex-row transition-all duration-300 ">
