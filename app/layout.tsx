48618000752bd450cb532b379886932354e20a82
Davi <davimachadocruz@gmail.com>
2024-04-10 05:48:35 -0300

    Alteração de FavIcon

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <title>SimpleFinance</title>
       </head>
       <body>
