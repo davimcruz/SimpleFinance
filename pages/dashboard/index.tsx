49f518cac9452060a68fb6f70cc28bd55eb85f26
Davi <davimachadocruz@gmail.com>
2024-04-16 18:35:44 -0300

    Teste

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -30,7 +30,7 @@ const DashboardPage = () => {
     const fetchUserData = async () => {
       try {
         const response = await fetch(
-          `/api/Queries/query?email=davimachadocruz@gmail.com`
+          `/api/Queries/query?email=${emailFromCookie}`
         )
         if (!response.ok) {
           throw new Error("Erro ao obter dados do usuário")
