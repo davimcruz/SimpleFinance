b88c15a74ffd12eb8d2bb7070e3eaaaa53fb7552
davimcruz <davimachadocruz@gmail.com>
2024-10-07 16:38:12 -0300

    chore(vscode): atualizar /app para SEO e avisos de manutenabilidade

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "Simple Finance - Gerencie suas Finanças Facilmente",
+  title: "Simple Finance",
   description:
-    "Descubra o Simple Finance, o principal software de finanças pessoais projetado para ajudá-lo a gerenciar orçamentos, despesas e investimentos em um só lugar. Comece a controlar seu futuro financeiro hoje.",
+    "Descubra o Simple Finance, o principal software de finanças pessoais projetado para ajudá-lo a gerenciar orçamentos, despesas e cartões em um só lugar. Comece a controlar seu futuro financeiro hoje.",
   keywords: [
     "software de finanças pessoais",
     "gerenciamento de orçamento",
     "acompanhamento de despesas",
-    "portfólio de investimentos",
+    "cartão de crédito",
     "planejamento financeiro",
     "Simple Finance",
     "gestão de dinheiro",
@@ -18,14 +18,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Simple Finance - Seu Gerente de Finanças Pessoais",
     description:
-      "Simplifique seu gerenciamento financeiro com o Simple Finance. Acompanhe despesas, gerencie orçamentos e supervisione seus investimentos com facilidade.",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@SimpleFinance",
-    title: "Simple Finance - Software de Finanças Pessoais",
-    description:
-      "Gerencie suas finanças sem esforço com o Simple Finance. Da elaboração de orçamentos ao acompanhamento de investimentos, obtenha tudo o que precisa para tomar controle da sua vida financeira.",
+      "Simplifique seu gerenciamento financeiro com o Simple Finance. Acompanhe despesas, gerencie orçamentos e supervisione seus cartões com facilidade.",
   },
 }
 
